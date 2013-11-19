7ab77145ba5819ba7a2d52478beacf6f330f99df
maksym.barchynskyi <max@ubuntu.(none)>
2013-11-19 15:45:26 +0200

    Add special registration window for Restorator.

--- src/RegistrationWindow.py
+++ src/RegistrationWindow.py
@@ -15,35 +15,40 @@ class RegistrationWindow(QWidget):
         super(RegistrationWindow,self).__init__()
 
         self.dbm = DB_Manager()
+        self.setWindowTitle("Registration")
 
         self.ok_btn = QPushButton("Register")
         self.close_btn = QPushButton("Close")
 
-        if user_type == 0:
-            self.user_type_map = {0:"restorator",1:"seller",2:"customer"}
-            self.user_type = user_type
+        self.layout = QGridLayout()
 
-            self.name_lbl = QLabel("Enter name:")
-            self.name_e = QLineEdit()
-            self.pass_lbl = QLabel("Enter pass:")
+        self.user_type_map = {0:"restorator",1:"seller",2:"customer"}
+        self.user_type = user_type
+
+        self.name_lbl = QLabel("Enter name:")
+        self.name_e = QLineEdit()
+        self.pass_lbl = QLabel("Enter pass:")
+        self.pass_e = QLineEdit()
+
+        self.layout.addWidget(self.name_lbl)
+        self.layout.addWidget(self.name_e)
+        self.layout.addWidget(self.pass_lbl)
+        self.layout.addWidget(self.pass_e)
+
+        if user_type == 0: # add special field if user is restorator
             self.restoran_name_lbl = QLabel("Restoran name:")
             self.restoran_name_e = QLineEdit()
             self.restoran_location_lbl = QLabel("Restoran location:")
             self.restoran_location_e = QLineEdit()
-            self.pass_e = QLineEdit()
+            self.places_count_lbl = QLabel("Restoran places:")
+            self.places_count_e = QLineEdit()
 
-            #self.ok_btn = QPushButton("Register")
-            #self.close_btn = QPushButton("Close")
-
-            self.layout = QGridLayout()
-            self.layout.addWidget(self.name_lbl)
-            self.layout.addWidget(self.name_e)
-            self.layout.addWidget(self.pass_lbl)
-            self.layout.addWidget(self.pass_e)
             self.layout.addWidget(self.restoran_name_lbl)
             self.layout.addWidget(self.restoran_name_e)
             self.layout.addWidget(self.restoran_location_lbl)
             self.layout.addWidget(self.restoran_location_e)
+            self.layout.addWidget(self.places_count_lbl)
+            self.layout.addWidget(self.places_count_e)
 
 
         self.layout.addWidget(self.ok_btn)
