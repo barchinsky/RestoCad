092b8fa9316b5c4ff282f1b3da8cae97c381f6ed
maksym.barchynskyi <max@ubuntu.(none)>
2013-11-19 16:56:00 +0200

    Added loading restoran list and choosen restoran menu.

--- src/CustomerMainWindow.py
+++ src/CustomerMainWindow.py
@@ -2,30 +2,34 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from DB_Manager import DB_Manager
 
 class CustomerMainWindow(QWidget):
     def __init__(self):
         super(CustomerMainWindow,self).__init__()
+        self.dbm = DB_Manager()
 
         self.setWindowTitle("Custormer")
         self.restoran_list_lbl = QLabel("Choose restoran:")
         self.restoran_list = QComboBox()
         self.menu_l_lbl = QLabel("Look at menu:")
-        self.menu_l = QComboBox()
+        self.menu_l_c = QComboBox()
         self.restoran_location_lbl = QLabel("Restoran location:")
         self.restoran_location_e = QLineEdit()
         self.order_btn = QPushButton("Order!")
         self.restoran_rate_lbl = QLabel("Restoran rate:")
         self.restoran_rate_e = QLineEdit()
         self.rate_btn = QPushButton("Rate!") # shows window for restoran rate
-        self.logout_btn = QPushButton("Leave")
+        self.logout_btn = QPushButton("Exit")
+
+        self.load()
 
         self.layout = QGridLayout()
 
         self.layout.addWidget(self.restoran_list_lbl)
         self.layout.addWidget(self.restoran_list)
         self.layout.addWidget(self.menu_l_lbl)
-        self.layout.addWidget(self.menu_l)
+        self.layout.addWidget(self.menu_l_c)
         self.layout.addWidget(self.restoran_location_lbl)
         self.layout.addWidget(self.restoran_location_e)
         self.layout.addWidget(self.order_btn)
@@ -38,10 +42,40 @@ class CustomerMainWindow(QWidget):
 
         self.connect(self.logout_btn,SIGNAL("clicked()"),self,SLOT("close()"))
         self.connect(self.order_btn,SIGNAL("clicked()"),self,SLOT("order()"))
+        self.connect(self.restoran_list,SIGNAL("currentIndexChanged(int)"),self, SLOT("load_restoran_menu()") )
 
     @pyqtSlot()
     def order(self):
         print "CustomerWindow::order()::Order called"
+
+    def load(self):
+        self.load_restoran_list()
+        self.load_restoran_menu()
+
+    def load_restoran_list(self):
+        restoran_list = self.dbm.get_restoran_list()
+
+        for restoran in restoran_list:
+            self.restoran_list.addItem(restoran)
+
+    @pyqtSlot()
+    def load_restoran_menu(self):
+        print "Load menu called."
+        self.clean_menu_combo()
+        restoran = str(self.restoran_list.currentText())
+        menu = self.dbm.get_restoran_menu(restoran)
+        print "current text: %s"%(restoran)
+
+        for dish in menu:
+            self.menu_l_c.addItem(dish)
+
+    def clean_menu_combo(self):
+        print "Clean combo"
+        print (self.menu_l_c.count())
+        for i in range(self.menu_l_c.count()):
+            self.menu_l_c.removeItem(i)
+
+        
 
 
 
